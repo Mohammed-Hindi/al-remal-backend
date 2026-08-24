0701773b2599a5aa1baa8cfb9bdf2ef8200c77c4
Mohammed Hindi <moh5408415@gmail.com>
2026-08-24 13:51:22 +0300

    Add AdminSeeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@elremal.com',
+            'password' => 'password123',
+            'must_change_password' => false,
+        ]);
+    }
+}
